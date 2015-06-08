82009f06e51cbdf6f36645af06662ce0fc4ad557
severin <severin@sverdvik.no>
2015-06-08 10:22:24 +0200

    oppgave1

---
+++ MVCintro/Controllers/PersonController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCintro.Models;
+
+namespace MVCintro.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Show(int id = 1)
+        {
+            var model = new PersonModel(id, "Nils", "Jensen", "9161166");
+
+            return View(model);
+        }
+    }
+}
---
+++ MVCintro/Models/PersonModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCintro.Models
+{
+    public class PersonModel
+    {
+        public PersonModel(int personId, string firstName, string lastName, string phoneNumber)
+        {
+            Id = personId;
+            FirstName = firstName;
+            LastName = lastName;
+            PhoneNumber = phoneNumber;
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
